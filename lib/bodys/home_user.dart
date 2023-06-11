597b3360ac8857e9de340ea952bb1a44d079b4c0
ung <test@abc.com>
2023-06-11 18:25:29 +0700

    update

--- lib/bodys/home_user.dart
+++ lib/bodys/home_user.dart
@@ -1,4 +1,8 @@
+import 'package:blindhelp/utility/app_constant.dart';
+import 'package:blindhelp/widgets/widget_circle_image.dart';
+import 'package:blindhelp/widgets/widget_form.dart';
 import 'package:blindhelp/widgets/widget_text.dart';
+import 'package:blindhelp/widgets/widget_title.dart';
 import 'package:flutter/material.dart';
 
 class HomeUser extends StatelessWidget {
@@ -6,6 +10,150 @@ class HomeUser extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const WidgetText(data: 'HomeUser');
+    return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              decoration: AppConstant().borderBox(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  WidgetCircleImage(
+                    radius: 45,
+                  ),
+                  SizedBox(
+                    width: 218,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        WidgetTitle(title: 'ชื่อ-นามสกุล :'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            WidgetForm(
+                              width: 100,
+                              marginTop: 0,
+                            ),
+                            WidgetForm(
+                              width: 100,
+                              marginTop: 0,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  WidgetText(data: 'data'),
+                                  WidgetForm(
+                                    width: 60,
+                                    marginTop: 0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  WidgetText(data: 'data'),
+                                  WidgetForm(
+                                    width: 60,
+                                    marginTop: 0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  WidgetText(data: 'data'),
+                                  WidgetForm(
+                                    width: 60,
+                                    marginTop: 0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  WidgetText(data: 'data'),
+                                  WidgetForm(
+                                    width: 60,
+                                    marginTop: 0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  WidgetText(data: 'data'),
+                                  WidgetForm(
+                                    width: 60,
+                                    marginTop: 0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  WidgetText(data: 'data'),
+                                  WidgetForm(
+                                    width: 60,
+                                    marginTop: 0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
   }
 }
