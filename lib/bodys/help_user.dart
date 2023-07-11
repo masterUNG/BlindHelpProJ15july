b945d7dbe931b11b3d076b6a50814a30990347e4
ung <test@abc.com>
2023-07-11 18:14:30 +0700

    Chat Success

--- lib/bodys/help_user.dart
+++ lib/bodys/help_user.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:blindhelp/models/chat_model.dart';
+import 'package:blindhelp/utility/app_constant.dart';
 import 'package:blindhelp/utility/app_controller.dart';
 import 'package:blindhelp/utility/app_service.dart';
 import 'package:blindhelp/widgets/widget_button.dart';
 import 'package:blindhelp/widgets/widget_form.dart';
 import 'package:blindhelp/widgets/widget_text.dart';
+import 'package:chat_bubbles/bubbles/bubble_special_three.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -19,6 +23,41 @@ class _HelpUserState extends State<HelpUser> {
   TextEditingController textEditingController = TextEditingController();
   AppController appController = Get.put(AppController());
 
+  late StreamSubscription<QuerySnapshot<Map<String, dynamic>>>
+      streamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    readChat();
+  }
+
+  @override
+  void dispose() {
+    streamSubscription.cancel();
+    super.dispose();
+  }
+
+  void readChat() {
+    streamSubscription = FirebaseFirestore.instance
+        .collection('user')
+        .doc(appController.userModelLogins.last.uid)
+        .collection('chat').orderBy('timestamp')
+        .snapshots()
+        .listen((event) {
+      if (appController.chatModels.isNotEmpty) {
+        appController.chatModels.clear();
+      }
+
+      if (event.docs.isNotEmpty) {
+        for (var element in event.docs) {
+          ChatModel chatModel = ChatModel.fromMap(element.data());
+          appController.chatModels.add(chatModel);
+        }
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +67,24 @@ class _HelpUserState extends State<HelpUser> {
           child: SizedBox(
             width: boxConstraints.maxWidth,
             height: boxConstraints.maxWidth - 60,
-            child: const WidgetText(data: 'HelpUser'),
+            child: Obx(() {
+              return appController.chatModels.isEmpty
+                  ? const SizedBox()
+                  : ListView.builder(reverse: true,
+                      itemCount: appController.chatModels.length,
+                      itemBuilder: (context, index) => BubbleSpecialThree(
+                        text: appController.chatModels[index].message,
+                        color: AppConstant.blue,
+                        tail: true,
+                        isSender: appController.userModelLogins.last.uid ==
+                            appController.chatModels[index].uidSender,
+                        textStyle: Theme.of(context)
+                            .textTheme
+                            .titleSmall!
+                            .copyWith(color: Colors.white),
+                      ),
+                    );
+            }),
           ),
         );
       }),
@@ -53,7 +109,7 @@ class _HelpUserState extends State<HelpUser> {
                     urlSender:
                         appController.userModelLogins.last.urlAvatar ?? '',
                     timestamp: Timestamp.fromDate(DateTime.now()));
-                print('chatModel --> ${chatModel.toMap()}');
+
                 AppService()
                     .addChat(map: chatModel.toMap())
                     .then((value) => textEditingController.text = '');
