60c8ecc2a55e127ab28412800310a91202b4831b
masterUNG <masterUng@abc.com>
2023-10-31 17:12:02 +0700

    update

--- lib/states/qr_scan_page.dart
+++ lib/states/qr_scan_page.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: avoid_print
 
+import 'dart:io';
+
 import 'package:blindhelp/states/display_result_qr_scan.dart';
+import 'package:blindhelp/utility/app_constant.dart';
 import 'package:blindhelp/widgets/widget_icon_button.dart';
+import 'package:blindhelp/widgets/widget_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:scan/scan.dart';
 
 class QrScanPage extends StatefulWidget {
@@ -38,25 +43,75 @@ class _QrScanPageState extends State<QrScanPage> {
             Positioned(
               right: 16,
               top: 16,
-              child: WidgetIconButton(
-                iconData: Icons.flash_on,
-                pressFunc: () {
-                  scanController.toggleTorchMode();
-                },
-                size: 36,
-                color: Colors.white,
+              child: Column(mainAxisSize: MainAxisSize.min,
+                children: [
+                  WidgetIconButton(
+                    iconData: Icons.flash_on,
+                    pressFunc: () {
+                      scanController.toggleTorchMode();
+                    },
+                    size: 36,
+                    color: Colors.white,
+                  ),
+                   WidgetText(
+                    data: 'เปิดแฟลซ',
+                    textStyle: AppConstant()
+                        .titleStyle(context: context, color: Colors.white),
+                  ),
+                ],
               ),
             ),
             Positioned(
               left: 16,
               top: 16,
-              child: WidgetIconButton(
-                iconData: Icons.clear,
-                pressFunc: () {
-                  Get.back();
-                },
-                size: 36,
-                color: Colors.white,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  WidgetIconButton(
+                    iconData: Icons.clear,
+                    pressFunc: () {
+                      Get.back();
+                    },
+                    size: 36,
+                    color: Colors.white,
+                  ),
+                  WidgetText(
+                    data: 'ปิด',
+                    textStyle: AppConstant()
+                        .titleStyle(context: context, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              right: 16,
+              bottom: 16,
+              child: Column(mainAxisSize: MainAxisSize.min,
+                children: [
+                  WidgetIconButton(
+                    iconData: Icons.photo_library,
+                    pressFunc: () async {
+                      var result = await ImagePicker()
+                          .pickImage(source: ImageSource.gallery);
+                      if (result != null) {
+                        File file = File(result.path);
+                        await Scan.parse(file.path).then((value) {
+                          print('##30oct value scan ---> $value');
+                          if (value != null) {
+                            Get.off(DisplayResultQRscan(resultQR: value));
+                          }
+                        });
+                      }
+                    },
+                    size: 36,
+                    color: Colors.white,
+                  ),
+                   WidgetText(
+                    data: 'สแกนจากภาพ',
+                    textStyle: AppConstant()
+                        .titleStyle(context: context, color: Colors.white),
+                  ),
+                ],
               ),
             ),
           ],
